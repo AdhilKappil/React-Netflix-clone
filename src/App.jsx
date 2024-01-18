4d8cb63214c768c373a0cc59b5c50c77efeb955a
AdhilKappil <adhilalikappil@gmail.com>
2024-01-18 19:40:56 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+ import Baner from "./Componets/Baner/Baner"
+
+function App() {
+  return (
+    <div>
+      <Baner/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Componets/Baner/Baner.jsx
@@ -0,0 +1,26 @@
+
+
+function Baner() {
+    return (
+        <div className="banner flex items-center justify-between">
+            <div className="flex space-x-4">
+                <ul className="flex space-x-4 list-none">
+                <div className="p-10">
+                </div>
+                    <li>Home</li>
+                    <li>TV Shows</li>
+                    <li>Movies</li>
+                    <li>News & Popular</li>
+                    <li>My list</li>
+                    <li>Browse by Languges</li>
+                </ul>
+                <div>
+                    
+                </div>
+            </div>
+            <div></div>
+        </div>
+    )
+}
+
+export default Baner
